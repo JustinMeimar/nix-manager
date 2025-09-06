9f9865df38f1da9035a84c9525368e88c711eb06
Justin <meimar@ualberta.ca>
2025-09-05 19:02:58 -0600

    update: ssh configs

--- programs/nvim/plugins/treesitter.nix
+++ programs/nvim/plugins/treesitter.nix
@@ -6,6 +6,7 @@
 
       settings = {
         ensureInstalled = [
+          "antlr"
           "bash"
           "c"
           "cpp"
--- programs/ssh.nix
+++ programs/ssh.nix
@@ -1,69 +1,84 @@
-{ config, lib, pkgs, ... } : {
+{ config, lib, pkgs, ... } : 
+
+let
+  
+  mkKeyConfig = { user, hostname, key }: {
+    user = user;
+    hostname = hostname;
+    identityFile = key;
+    identitiesOnly = true;
+  };
+  
+  mkPwdConfig = { user, hostname ? null, proxyJump ? null }: {
+    user = user;
+    extraOptions = {
+      PreferredAuthentications = "password";
+      PubkeyAuthentication = "no";
+    };
+  } // lib.optionalAttrs (hostname != null) {
+    hostname = hostname;
+  } // lib.optionalAttrs (proxyJump != null) {
+    proxyJump = proxyJump;
+  };
+in
+{
   programs.ssh = {
-   enable = true;
+    enable = true;
    
-   matchBlocks = {
-     "innis" = {
-       user = "meimar";
-       hostname = "innisfree.cs.ualberta.ca";
-     };
-     
-     "cdol04" = {
-       user = "meimar";
-       proxyJump = "innis";
-       extraOptions = {
-         PreferredAuthentications = "password";
-         PubkeyAuthentication = "no";
-       };
-     };
-     
-     "cdol03" = {
-       user = "meimar";
-       proxyJump = "innis";
-       extraOptions = {
-         PreferredAuthentications = "password";
-         PubkeyAuthentication = "no";
-       };
-     };
-     
-     "cdol02" = {
-       user = "meimar";
-       proxyJump = "innis";
-       extraOptions = {
-         PreferredAuthentications = "password";
-         PubkeyAuthentication = "no";
-       };
-     };
+    matchBlocks = {
       
-     "ug" = {
-       user = "cmput415";
-       hostname = "uf17.cs.ualberta.ca";
-       identityFile = "~/.ssh/cmput415";
-     };
+      "innis" = mkKeyConfig {
+        user = "meimar";
+        hostname = "innisfree.cs.ualberta.ca";
+        key = "~/.ssh/cdol.pub";
+      };
 
-     "ci415" = {
-       user = "meimar";
-       hostname = "ci.cmput415.cs.ualberta.ca";
-       proxyJump = "innis";
-       identityFile = "~/.ssh/ci-server";
-     };
-     
-     "bee" = {
-       user = "justin";
-       hostname = "192.168.1.88";
-       identityFile = "~/.ssh/justin-bee";
-     };
-     
-     "github.com" = {
-       hostname = "github.com";
-       identityFile = "~/.ssh/github-zen";
-     };
+      "coronation" = mkKeyConfig {
+        user = "cmput415";
+        hostname = "coronation.cs.ualberta.ca";
+        key = "~/.ssh/cmput415";
+      };
 
-     "gazbolt" = {
-       user = "justin";
-       hostname = "159.223.200.125";
-       identityFile = "~/.ssh/github-zen";
-     };
+      "bee" = mkKeyConfig {
+        user = "justin";
+        hostname = "192.168.1.88";
+        key = "~/.ssh/justin-bee";
+      };
+
+      "gazbolt" = mkKeyConfig {
+        user = "justin";
+        hostname = "159.223.200.125";
+        key = "~/.ssh/github-zen";
+      };
+
+      "cdol04" = mkPwdConfig {
+        user = "meimar";
+        proxyJump = "innis";
+      };
+      
+      "cdol03" = mkPwdConfig {
+        user = "meimar";
+        proxyJump = "innis";
+      };
+      
+      "uofa" = mkPwdConfig {
+        user = "meimar";
+        hostname = "coronation.cs.ualberta.ca";
+      };
+ 
+      "ci415" = {
+        user = "meimar";
+        hostname = "ci.cmput415.cs.ualberta.ca";
+        proxyJump = "innis";
+        identityFile = "~/.ssh/ci-server";
+        identitiesOnly = true;
+      };
+      
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/github-zen";
+        identitiesOnly = true;
+      };  
    };
   };
 }
